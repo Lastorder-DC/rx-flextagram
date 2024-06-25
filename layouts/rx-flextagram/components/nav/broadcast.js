5617b4782da9ebef16f8dbaa67820360c0dd60ce
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2024-06-25 13:56:24 +0900

    js 분리

---
+++ layouts/rx-flextagram/components/nav/broadcast.js
@@ -0,0 +1,23 @@
+function refreshStylesheet(url, interval) {
+    let link = document.querySelector(`link[href="${url}"]`);
+    
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = url;
+        document.head.appendChild(link);
+    }
+    
+    setInterval(() => {
+        const newLink = document.createElement('link');
+        newLink.rel = 'stylesheet';
+        newLink.href = `${url}?t=${new Date().getTime()}`;
+
+        newLink.onload = () => {
+            link.parentNode.removeChild(link);
+            link = newLink;
+        };
+
+        document.head.appendChild(newLink);
+    }, interval);
+}
